dc517ea0f7f9198a670592e98bbbdbefee452403
Daroas <166027977+Daroas@users.noreply.github.com>
2024-04-04 23:28:24 -0300

    Update InstructionsComponent.jsx

--- components/InstructionsComponent.jsx
+++ components/InstructionsComponent.jsx
@@ -14,39 +14,12 @@ export default function InstructionsComponent() {
 				</p>
 			</header>
 
-			<div className={styles.buttons_container}>
-				<a
-					target={"_blank"}
-					href={"https://createweb3dapp.alchemy.com/#components"}
-				>
+			
 					<div className={styles.button}>
-						{/* <img src="https://static.alchemyapi.io/images/cw3d/Icon%20Medium/lightning-square-contained-m.svg" width={"20px"} height={"20px"} /> */}
-						<p>Add Components</p>
-					</div>
-				</a>
-				<a
-					target={"_blank"}
-					href={"https://createweb3dapp.alchemy.com/#templates"}
-				>
-					<div className={styles.button}>
-						{/* <img src="public/desktop_1280x800_PNG.png" width={"20px"} height={"20px"} /> */}
+						{/* <img src="public/desktop_1280x800_PNG.png" width={"1280px"} height={"800px"} /> */}
 		
 					</div>
-				</a>
-				<a
-					target={"_blank"}
-					href={"https://docs.alchemy.com/docs/create-web3-dapp"}
-				>
-					<div className={styles.button}>
-						<img
-							src="https://static.alchemyapi.io/images/cw3d/Icon%20Large/file-eye-01-l.svg"
-							width={"20px"}
-							height={"20px"}
-						/>
-						<p>Visit Docs</p>
-					</div>
-				</a>
-			</div>
+			
 			<div className={styles.footer}>
 				<a href="https://alchemy.com/?a=create-web3-dapp" target={"_blank"}>
 					<img
