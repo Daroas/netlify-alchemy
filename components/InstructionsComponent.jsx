14b5f0374105ef3ac790c3652172060cf2b41872
Daroas <166027977+Daroas@users.noreply.github.com>
2024-04-04 23:34:35 -0300

    Update InstructionsComponent.jsx

--- components/InstructionsComponent.jsx
+++ components/InstructionsComponent.jsx
@@ -20,16 +20,7 @@ export default function InstructionsComponent() {
 		
 					
 			
-			<div className={styles.footer}>
-				<a href="https://alchemy.com/?a=create-web3-dapp" target={"_blank"}>
-					<img
-						id="badge-button"
-						style={{ width: "240px", height: "53px" }}
-						src="https://static.alchemyapi.io/images/marketing/badgeLight.png"
-						alt="Alchemy Supercharged"
-					/>
-				</a>
-				<div className={styles.icons_container}>
+		
 					<div>
 						<a
 							href="https://www.facebook.com/ligceluba/"
